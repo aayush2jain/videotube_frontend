8aa0c23fe2a6fdec36c1b512d24c05c8cea9e583
aayush jain <aayushjain1290@gmail.com>
2024-09-29 20:01:43 +0530

    register

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -45,14 +45,17 @@ const Signup = () => {
     return (
         
         <div className='w-[100vw] h-[100vh] bg-black flex items-center justify-center'>
-            <div className='md:w-[30vw] h-[80vh] w-[80vw] md:h-[37vw] rounded-3xl flex flex-col bg-white'>
+            <div className='md:w-[30vw] h-[90vh] w-[80vw] md:h-[42vw] rounded-3xl flex flex-col bg-white'>
                 <h1 className='md:mt-[8vh] mt-[4vh]  my-4 text-xl  md:ml-[11vw] ml-[27vw] font-bold'>SIGN UP</h1>
                 <form onSubmit={submit} encType='multipart/form-data' className='mx-auto'>
                     {/* <input type='text' placeholder='UserName' onChange={(e) => setUsername(e.target.value)} className='py-[2vh] w-[10vw] mb-[2vh] mx-[2.5vw] sm:pl-4 rounded-xl bg-gray-200' /> */}
                     <input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} className='py-[2vh] md:w-[20vw] w-[60vw] md:h-[4vw] h-[8vh]  md:mb-[3vh] mb-[4vh] md:ml-[4.5vw] ml-[8vw] pl-4 rounded-xl bg-gray-200' />
                     <input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} className='py-[2vh]  md:w-[20vw]  w-[60vw] md:h-[4vw] h-[8vh]  md:mb-[23h] mb-[4vh] md:ml-[4.5vw]  ml-[8vw]  pl-4 rounded-xl bg-gray-200' />
-                    <input type='file' id='coverImage' onChange={(e) => handleFileChange(e, setCoverImage)} className='py-[2vh]  md:w-[20vw] md:h-[4vw] h-[9vh]  w-[60vw] md:mb-[3vh] mb-[4vh] md:ml-[4.5vw]  ml-[8vw]  pl-4 rounded-xl bg-gray-200' />
+                      <h1  className='mx-[2vw] pl-4 font-semibold'>ProfileImage:</h1>
                     <input type='file' id='profileImage' onChange={(e) => handleFileChange(e, setavatar)} className='py-[2vh]  md:w-[20vw] md:h-[4vw] h-[9vh]  w-[60vw] md:mb-[3vh] mb-[4vh] md:ml-[4.5vw]  ml-[8vw]  pl-4 rounded-xl bg-gray-200' />
+                     <h1  className='mx-[2vw] pl-4 font-semibold'>CoverImage:</h1>
+                    <input type='file' id='coverImage' onChange={(e) => handleFileChange(e, setCoverImage)} className='py-[2vh]  md:w-[20vw] md:h-[4vw] h-[9vh]  w-[60vw] md:mb-[3vh] mb-[4vh] md:ml-[4.5vw]  ml-[8vw]  pl-4 rounded-xl bg-gray-200' />
+                   
                     <button type='submit' className='width-[10vw] px-[2vw] md:ml-[10vw] ml-[26vw] py-3 bg-black text-white text-xl rounded-xl'>Sign up</button>
                 </form>
                 {/* <div className='flex flex-row rounded-xl border-black border-2 w-[25vw] ml-[2.5vw] mt-2'>
