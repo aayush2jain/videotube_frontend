3ca1c5e2250aa06e1c6ac438a581d596df092255
aayush jain <aayushjain1290@gmail.com>
2024-09-29 22:54:04 +0530

    final push

--- src/pages/Yourvideo.js
+++ src/pages/Yourvideo.js
@@ -1,20 +1,45 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 const Yourvideo = () => {
     const navigate = useNavigate();
   const [videos, setVideos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  //new thing
+  const [userid, setuserid] = useState('');
+  const [username, setusername] = useState('');
+  const [avatar, setavatar] = useState('');
+  const [coverImage, setcoverImage] = useState('');
     const handleVideoClick = (id) => {
     navigate('/video', { state: { id } });
   };
+
+   useEffect(() => {
+    const fetchData = async () => {
+      try {
+         const response = await axios.get('https://backend-five-zeta-26.vercel.app/video/show',{
+          withCredentials: true // Include cookies
+        });
+        console.log('response', response);
+        setavatar(response.data.avatar);
+        setcoverImage(response.data.coverImage);
+        setusername(response.data.username);
+        setuserid(response.data._id);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   useEffect(() => {
     const fetchVideos = async () => {
       try {
         const response = await axios.get('https://backend-five-zeta-26.vercel.app/video/show',{
-          withCredentials: true // Include cookies
+          withCredentials:true
         });
         console.log("data",response)
         setVideos(response.data);
@@ -32,11 +57,21 @@ const Yourvideo = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className='flex md:flex-wrap flex-nowrap md:flex-row flex-col bg-black text-white justify-around pb-[10vh]'>
+    <>
+    <div className='bg-black'>
+    <div className='w-full h-[20vw] bg-slate-500 relative object-cover '>
+     <img src={coverImage} alt='' className='md:h-[20vw] h-[40vw] w-full'></img>
+        <div className='md:w-[13vw] flex md:h-[13vw] w-[20vw] h-[20vw] rounded-full bg-red-600 absolute md:top-[13vw] top-[25vw] left-[1vw]'>
+        <img src={avatar} alt='' className='object-center rounded-full md:w-[13vw] md:h-[13vw] w-[20vw] h-[20vw] '></img>
+        <h1 className='ml-[3vw] text-white font-normal z-50 md:mt-[7vw] mt-[13vw]  text-2xl'>@username</h1>
+        </div>
+      </div>
+    
+    <div className='flex md:flex-wrap mt-[20vh] flex-col md:flex-row bg-black text-white justify-around items-center pb-[10vh] '>
     
         {videos.map((video, index) => (
-          <div key={index} className='h-[30vh] w-[20vw] bg-black rounded-2xl my-10 ' onClick={()=>handleVideoClick(video._id)}>
-            <img src={video.thumbnail} alt="" className='h-[30vh] w-[20vw] rounded-2xl'></img>
+          <div key={index} className='h-[30vh] md:w-[20vw] w-[90vw]  bg-black rounded-2xl my-10 ' onClick={()=>handleVideoClick(video._id)}>
+            <img src={video.thumbnail} alt="" className='h-[30vh] md:w-[20vw] w-[90vw] rounded-2xl'></img>
             <div className='flex flex-row pt-2'>
               <div>
                 <img src={video.uploaderavatar} alt="" className='h-[7vh] w-[7vh] rounded-full'></img>
@@ -52,6 +87,8 @@ const Yourvideo = () => {
         ))}
 
     </div>
+    </div>
+    </>
   );
 };
 
